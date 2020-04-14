d85a82a5a87ac0a4e55e044f06320bbbc4297b78
Sander Jochems <sanderjochems@hotmail.nl>
2020-04-14 12:21:36 +0200

    Update Main.java

--- src/nl/avans/vsoprj2/wordcrex/Main.java
+++ src/nl/avans/vsoprj2/wordcrex/Main.java
@@ -3,8 +3,7 @@ package nl.avans.vsoprj2.wordcrex;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    	System.out.print("Hello home!");
     }
 
 }
