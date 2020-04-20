ab325e0b23d32009b1abbfae41c2b7650effa8f4
Sander Jochems <sanderjochems@hotmail.nl>
2020-04-20 12:25:07 +0200

    Update WordCrex.java

--- src/main/java/nl/avans/vsoprj2/wordcrex/WordCrex.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/WordCrex.java
@@ -1,10 +1,28 @@
 package nl.avans.vsoprj2.wordcrex;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  */
-public class WordCrex {
+public class WordCrex extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch();
     }
 }
