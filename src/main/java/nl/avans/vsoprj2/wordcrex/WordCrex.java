a98ed1efe272df8aca1f4eacec0016096f08809a
Sander Jochems <sanderjochems@hotmail.nl>
2020-04-21 11:58:06 +0200

    Update WordCrex.java

--- src/main/java/nl/avans/vsoprj2/wordcrex/WordCrex.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/WordCrex.java
@@ -1,31 +1,37 @@
 package nl.avans.vsoprj2.wordcrex;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
  * Hello world!
  */
 public class WordCrex extends Application {
+    public static final boolean DEBUG_MODE = true;
+
     @Override
     public void start(Stage stage) throws Exception {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-
+        stage.setTitle("WordCrex");
         stage.getIcons().add(new Image("/images/icon.png"));
 
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Parent sceneBox = new FXMLLoader(getClass().getResource("/views/login.fxml")).load();
+        Scene scene = new Scene(sceneBox);
+
+//        if (DEBUG_MODE) {
+//            stage.setTitle(String.format("WordCrex - Java: %s - JavaFX: %s", System.getProperty("java.version"), System.getProperty("javafx.version")));
+//        }
+
         stage.setScene(scene);
 
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
