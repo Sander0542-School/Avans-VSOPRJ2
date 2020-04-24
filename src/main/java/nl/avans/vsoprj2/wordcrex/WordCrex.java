5fc15ea67da533aa3aa1690c163e71fa0886bc27
Tommy Hosewol <t.hosewol@gmail.com>
2020-04-24 10:46:40 +0200

    Update src/main/java/nl/avans/vsoprj2/wordcrex/WordCrex.java
    Co-Authored-By: Sander Jochems <sanderjochems@hotmail.nl>

--- src/main/java/nl/avans/vsoprj2/wordcrex/WordCrex.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/WordCrex.java
@@ -18,7 +18,7 @@ public class WordCrex extends Application {
         stage.setTitle("WordCrex");
         stage.getIcons().add(new Image("/images/icon.png"));
 
-        Parent sceneBox = new FXMLLoader(getClass().getResource("/views/scoreboard.fxml")).load();
+        Parent sceneBox = new FXMLLoader(getClass().getResource("/views/login.fxml")).load();
         Scene scene = new Scene(sceneBox);
 
 //        if (DEBUG_MODE) {
