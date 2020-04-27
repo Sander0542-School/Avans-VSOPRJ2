3c90be4514e42df43f08cbbb4d2a09f8886034bc
Sander Jochems <sanderjochems@hotmail.nl>
2020-04-27 13:37:41 +0200

    Implement BackButton in Login view

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/LoginController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/LoginController.java
@@ -1,4 +1,9 @@
 package nl.avans.vsoprj2.wordcrex.controllers;
 
+import javafx.event.Event;
+
 public class LoginController extends Controller {
+    public void backButton(Event event) {
+        navigateTo("/views/index.fxml");
+    }
 }
