1548da0aba47906b4c73996f57d7b6e409f0b33a
Tommy Hosewol <t.hosewol@gmail.com>
2020-04-27 14:03:26 +0200

    Add User Model

---
+++ src/main/java/nl/avans/vsoprj2/wordcrex/models/User.java
@@ -0,0 +1,24 @@
+package nl.avans.vsoprj2.wordcrex.models;
+
+public class User {
+    // Properties
+    private String username;
+    private String password;
+
+    // Methods
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
