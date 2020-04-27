8f37368b8ee3e10edeaeb23f2dce537f924bad9b
Sander Jochems <sanderjochems@hotmail.nl>
2020-04-27 13:44:25 +0200

    Remove Event from LoginController@backButton

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/LoginController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/LoginController.java
@@ -3,7 +3,7 @@ package nl.avans.vsoprj2.wordcrex.controllers;
 import javafx.event.Event;
 
 public class LoginController extends Controller {
-    public void backButton(Event event) {
+    public void backButton() {
         navigateTo("/views/index.fxml");
     }
 }
