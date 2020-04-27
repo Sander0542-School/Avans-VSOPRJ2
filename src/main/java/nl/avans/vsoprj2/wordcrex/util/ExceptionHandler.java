1b3f547474d54eef083aa89c0b24ecc7dac9aa8e
Sander Jochems <sanderjochems@hotmail.nl>
2020-04-27 15:22:43 +0200

    Create ExceptionHandler

---
+++ src/main/java/nl/avans/vsoprj2/wordcrex/util/ExceptionHandler.java
@@ -0,0 +1,8 @@
+package nl.avans.vsoprj2.wordcrex.util;
+
+public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+
+    }
+}
