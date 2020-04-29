aa065d3cd8a80b385a34c906103901c4f1ad7f87
daanh432 <daan@daanhendriks.nl>
2020-04-29 13:14:26 +0200

    Capture enter / submit event on chat input textarea

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/ChatController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/ChatController.java
@@ -1,21 +1,28 @@
 package nl.avans.vsoprj2.wordcrex.controllers.game;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
 import nl.avans.vsoprj2.wordcrex.controls.gamechat.ChatRow;
+import nl.avans.vsoprj2.wordcrex.models.ChatMessage;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
-public class ChatController extends Controller {
-    private List<ChatRow> chatMessages = new ArrayList<ChatRow>();
+public class ChatController extends Controller implements Initializable {
+    private List<ChatMessage> chatMessages = new ArrayList<ChatMessage>();
 
     @FXML
     private TextArea chatMessageInput;
@@ -33,17 +40,35 @@ public class ChatController extends Controller {
             ResultSet resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
-                ChatRow chatRow = new ChatRow();
-                chatRow.setText(resultSet.getString("message"));
-                chatRow.setLeft(false); //TODO Check if username is same as current user (requires model te be finished for authentication)
-                chatMessages.add(chatRow);
+                ChatMessage chatMessage = new ChatMessage(resultSet.getString("username"), resultSet.getDate("moment"), resultSet.getString("message"));
+                chatMessages.add(chatMessage);
             }
         } catch (SQLException e) {
             //TODO Handle error
         }
     }
 
-    public void initialize() {
-        chatMessagesContainer.getChildren().addAll(chatMessages);
+    private void sendMessageHandler(KeyEvent keyEvent) {
+        String currentUserName = "jagermeester"; //TODO Convert to user model
+
+        if (keyEvent.getCode() == KeyCode.ENTER && !keyEvent.isShiftDown()) {
+            if (keyEvent.getEventType() == KeyEvent.KEY_RELEASED) {
+                System.out.println("Enter pressed, TODO Send message");
+            }
+            keyEvent.consume();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String currentUserName = "jagermeester"; //TODO Convert to user model
+
+        List<ChatRow> chatRows = chatMessages.stream().map(chatMessage -> new ChatRow(chatMessage.getMessage(), currentUserName.equals(chatMessage.getUsername()))).collect(Collectors.toList());
+
+        chatMessagesContainer.getChildren().addAll(chatRows);
+
+        chatMessageInput.addEventFilter(KeyEvent.KEY_PRESSED, this::sendMessageHandler);
+        chatMessageInput.addEventFilter(KeyEvent.KEY_RELEASED, this::sendMessageHandler);
     }
 }
+
