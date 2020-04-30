7275d09b4f117c0edd2fdd7a2e51e5b57eeb0f68
Sander Jochems <sanderjochems@hotmail.nl>
2020-04-30 14:51:41 +0200

    Create Abstract Model

---
+++ src/main/java/nl/avans/vsoprj2/wordcrex/models/Model.java
@@ -0,0 +1,10 @@
+package nl.avans.vsoprj2.wordcrex.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class Model {
+    public Model(ResultSet resultSet) throws SQLException {
+
+    }
+}
