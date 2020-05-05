34b6540aa4c7c5ed9f9b98735159eb427f54f5aa
Joris Koevoets <joriskoevoets@hotmail.com>
2020-05-05 11:47:19 +0200

    Add method checkWord

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
@@ -1,6 +1,28 @@
 package nl.avans.vsoprj2.wordcrex.controllers.game;
 
+import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class BoardController extends Controller {
+
+    public boolean checkWord(String word, String letterSet) {
+        Connection connection = Singleton.getInstance().getConnection();
+        PreparedStatement statement;
+
+        String query = "SELECT EXISTS(SELECT * FROM dictionary WHERE word = '" + word + "' AND letterset_code = '" + letterSet + "' AND state = 'accepted');";
+        try {
+            statement = connection.prepareStatement(query);
+            ResultSet result = statement.executeQuery();
+            result.next();
+            return result.getBoolean(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
