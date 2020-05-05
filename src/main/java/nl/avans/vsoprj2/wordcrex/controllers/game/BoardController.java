62aded63a45a3a3894581a05759b02258348a963
Joris Koevoets <joriskoevoets@hotmail.com>
2020-05-05 12:37:43 +0200

    Improved query with setStrings

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
@@ -10,12 +10,14 @@ import java.sql.SQLException;
 
 public class BoardController extends Controller {
 
-    public boolean checkWord(String word, String letterSet) {
+    public boolean checkWord(String word, String letterSetCode) {
         Connection connection = Singleton.getInstance().getConnection();
         PreparedStatement statement;
-        String query = "SELECT EXISTS(SELECT * FROM dictionary WHERE word = '" + word + "' AND letterset_code = '" + letterSet + "' AND state = 'accepted');";
+        String query = "SELECT EXISTS(SELECT * FROM dictionary WHERE word = ? AND letterset_code = ? AND state = 'accepted');";
         try {
             statement = connection.prepareStatement(query);
+            statement.setString(1,word);
+            statement.setString(2,letterSetCode);
             ResultSet result = statement.executeQuery();
             result.next();
             return result.getBoolean(1);
