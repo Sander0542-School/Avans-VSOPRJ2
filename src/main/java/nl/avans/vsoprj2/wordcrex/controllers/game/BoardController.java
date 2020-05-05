020e1b0193d7e3e361091e4ba96c7cb280cc90f4
MaxvdVossenberg <maxvossenberg@gmail.com>
2020-05-05 12:22:03 +0200

    Cleaned up code
    Added exception handler.
    Removed unnesisary code.

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
@@ -2,13 +2,12 @@ package nl.avans.vsoprj2.wordcrex.controllers.game;
 
 import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
+import nl.avans.vsoprj2.wordcrex.exceptions.DbConnectionException;
 import nl.avans.vsoprj2.wordcrex.models.Account;
 import nl.avans.vsoprj2.wordcrex.models.Game;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class BoardController extends Controller {
     private Game model;
@@ -32,14 +31,13 @@ public class BoardController extends Controller {
 
             String query = "UPDATE game SET game_state = ?, username_winner = ? WHERE game_id = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1,"");
+            preparedStatement.setString(1,model.getGameState().toString());
             preparedStatement.setString(2,winner.getUsername());
             preparedStatement.setInt(3,model.getGameId());
             preparedStatement.executeUpdate();
 
-
         } catch(Exception ex) {
-
+            throw new DbConnectionException(ex);
         }
     }
 }
