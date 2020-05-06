1de22a260e1b3fa3c82b6a2d2b5668df75be3335
Tommy Hosewol <t.hosewol@gmail.com>
2020-05-06 14:54:29 +0200

    Load statistics from database and append them to the view

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/StatisticsController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/StatisticsController.java
@@ -2,9 +2,13 @@ package nl.avans.vsoprj2.wordcrex.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.models.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class StatisticsController extends Controller {
     @FXML
@@ -16,61 +20,65 @@ public class StatisticsController extends Controller {
     @FXML
     Label gamesLost;
 
+    @FXML
+    Label gamesTied;
+
+    @FXML
+    Label gamesLeft;
+
+    @FXML
+    Label topGameScore;
+
+    @FXML
+    Label topWordScore;
+
     private User user;
     Connection connection = null;
 
-    @FXML
-    private void initialize() {
-        try {
-            this.connection = DriverManager.getConnection("jdbc:mysql://tommyhosewol.com/avans_wordcrex?user=wordcrex&password=EdiILXhe1fK04mvA");
-        } catch (SQLException exception) {
-            System.err.println("SQLException: " + exception.getMessage());
-            System.err.println("SQLState: " + exception.getSQLState());
-            System.err.println("VendorError: " + exception.getErrorCode());
-        }
+    public void initialize() {
+        this.connection = Singleton.getInstance().getConnection();
 
-
-        // TEMP 4 USER
+        // TEMP USER UNTILL USER MODEL IS AVAIL
         user = new User();
         user.setUsername("Lidewij");
         user.setPassword("rrr");
 
-        getTableData();
-        setTableData();
+        setUser();
+        setStatistics();
     }
 
-    private void setTableData() {
+    private void setUser() {
         name.setText(user.getUsername());
-
     }
 
-    private void getTableData() {
+    private void setStatistics() {
         PreparedStatement stmt = null;
         ResultSet rs = null;
         String query = null;
 
         try {
-            query = "SELECT COUNT(`game_id`) as `games_won` FROM `game` WHERE `username_winner` = ?;";
+            query = "SELECT (SELECT COUNT(`game_id`) FROM `game` WHERE `username_winner` = ?) as games_won," +
+                    " (SELECT COUNT(`game_id`) FROM `game` WHERE `username_winner` != ? AND `username_player1` = ? OR `username_player2` = ? AND `username_winner` IS NOT NULL) as games_lost," +
+                    " (SELECT COUNT(`game_id`) FROM `game` WHERE `username_player1` = ? OR `username_player2` = ? AND `game_state` = 'finished' AND `username_winner` IS NULL) as games_tied," +
+                    " (SELECT COUNT(`game_id`) FROM `game` WHERE `username_winner` != ? AND `game_state` = 'resigned') as games_left," +
+                    " (SELECT GREATEST(IFNULL((SELECT MAX(`score1`) FROM `score` WHERE `username_player1` = ? AND `game_state` = 'finished'),0), IFNULL((SELECT MAX(`score2`) FROM `score` WHERE `username_player2` = ? AND `game_state` = 'finished'),0))) as top_game_score;";
             stmt = connection.prepareStatement(query);
-            stmt.setString(1, user.getUsername());
+
+            for (int i = 1; i <= stmt.getParameterMetaData().getParameterCount(); i++) {
+                stmt.setString(i, user.getUsername());
+            }
+
             rs = stmt.executeQuery();
 
             while (rs.next()) {
-                gamesWon.setText(rs.getString(1));
+                gamesWon.setText(rs.getString("games_won"));
+                gamesLost.setText(rs.getString("games_lost"));
+                gamesTied.setText(rs.getString("games_tied"));
+                gamesLeft.setText(rs.getString("games_left"));
+                topGameScore.setText(rs.getString("top_game_score"));
+                topWordScore.setText("0" /*rs.getString("top_game_score")*/);
             }
 
-            query = "SELECT COUNT(`game_id`) as `games_lost` FROM `game` WHERE `username_winner` != ? AND `username_player1` = ? OR `username_player2` = ?;";
-            stmt = connection.prepareStatement(query);
-            stmt.setString(1, user.getUsername());
-            stmt.setString(2, user.getUsername());
-            stmt.setString(3, user.getUsername());
-            rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                gamesLost.setText(rs.getString(1));
-            }
-
-            // Now do something with the ResultSet ....
         } catch (SQLException ex) {
             // handle any errors
             System.out.println("SQLException: " + ex.getMessage());
