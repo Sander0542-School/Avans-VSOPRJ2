f33944599dfe54b5828eb339434e462489d75734
Lars Rijnen <Larsrijnen@gmail.com>
2020-05-06 09:23:37 +0200

    Create newGamePage route

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/GamesController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/GamesController.java
@@ -1,4 +1,7 @@
 package nl.avans.vsoprj2.wordcrex.controllers;
 
 public class GamesController extends Controller {
+    public void newGamePage() {
+        navigateTo("/views/game/new.fxml");
+    }
 }
