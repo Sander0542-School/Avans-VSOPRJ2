eabde31d51f330eadd19da58134e6b34d0868a14
Lars Rijnen <Larsrijnen@gmail.com>
2020-05-06 20:10:25 +0200

    First version of connections

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/NewController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/NewController.java
@@ -1,22 +1,34 @@
 package nl.avans.vsoprj2.wordcrex.controllers.game;
 
+import javafx.fxml.FXML;
 import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
 import nl.avans.vsoprj2.wordcrex.exceptions.DbLoadException;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class NewController extends Controller {
         public NewController() {
-            Connection connection = Singleton.getInstance().getConnection();
-            Singleton.getInstance().getUser();
-            
-
-
+//            Connection connection = Singleton.getInstance().getConnection();
+//
+//            try {
+//                PreparedStatement statement = connection.prepareStatement("SELECT * FROM `account` WHERE username != 'luc'");
+//                ResultSet resultSet = statement.executeQuery();
+//
+//                System.out.println(resultSet);
+//
+//                //Singleton.getInstance().getUser();
+//            } catch (SQLException e) {
+//                throw new DbLoadException(e);
+//            }
 
             // get all players
             // return to page
         }
+
 
         public void randomGameRequest() {
             this.createGame("NL", "Lars");
@@ -29,13 +41,13 @@ public class NewController extends Controller {
         private boolean createGame(String letterset, String username2) {
             Boolean gameCreated = false;
 
-            try {
-                Connection connection = Singleton.getInstance().getConnection();
-                Singleton.getInstance().getUser();
-
-            } catch (Exception e) {
-                throw new DbLoadException(e);
-            }
+//            try {
+//                Connection connection = Singleton.getInstance().getConnection();
+//                Singleton.getInstance().getUser();
+//
+//            } catch (Exception e) {
+//                throw new DbLoadException(e);
+//            }
 
             return gameCreated;
         }
