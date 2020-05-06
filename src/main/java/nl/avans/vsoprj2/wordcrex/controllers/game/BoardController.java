bf6eb51b242c12e68b25411dd412b1464398548b
Joris Koevoets <63403671+Joris914@users.noreply.github.com>
2020-05-06 21:11:19 +0200

    Pass Game to method instead of letterSetCode
    Co-authored-by: Sander Jochems <sanderjochems@hotmail.nl>

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
@@ -11,12 +11,12 @@ import java.sql.SQLException;
 
 public class BoardController extends Controller {
 
-    public boolean checkWord(String word, String letterSetCode) {
+    public boolean checkWord(Game game, String word) {
         Connection connection = Singleton.getInstance().getConnection();
         try {
             PreparedStatement statement = connection.prepareStatement("SELECT EXISTS(SELECT * FROM dictionary WHERE word = ? AND letterset_code = ? AND state = 'accepted');");
             statement.setString(1, word);
-            statement.setString(2, letterSetCode);
+            statement.setString(2, game.getLettersetCode());
             ResultSet result = statement.executeQuery();
             result.next();
             return result.getBoolean(1);
