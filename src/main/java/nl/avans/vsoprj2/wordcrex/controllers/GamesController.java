252ef2d01a6330fbcb4b3a1d81951324a8736710
Lars Rijnen <Larsrijnen@gmail.com>
2020-05-06 12:37:15 +0200

    Start create new page

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/GamesController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/GamesController.java
@@ -1,7 +1,31 @@
 package nl.avans.vsoprj2.wordcrex.controllers;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import java.util.Optional;
+
 public class GamesController extends Controller {
     public void newGamePage() {
         navigateTo("/views/game/new.fxml");
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Game request");
+        alert.setHeaderText("Je kunt een nieuw spel starten met Sander0542");
+
+        ButtonType buttonTypeDecline = new ButtonType("Decline");
+        ButtonType buttonTypeAccept = new ButtonType("Accept");
+        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(buttonTypeDecline, buttonTypeAccept, buttonTypeCancel);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeDecline) {
+            System.out.println("Decline game request");
+        } else if (result.get() == buttonTypeAccept) {
+            System.out.println("Accept game request");
+        } else {
+            // Cancel alert popup
+        }
     }
 }
--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/NewController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/NewController.java
@@ -1,6 +1,42 @@
 package nl.avans.vsoprj2.wordcrex.controllers.game;
 
+import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
+import nl.avans.vsoprj2.wordcrex.exceptions.DbLoadException;
+
+import java.sql.Connection;
 
 public class NewController extends Controller {
+        public NewController() {
+            Connection connection = Singleton.getInstance().getConnection();
+            Singleton.getInstance().getUser();
+            
+
+
+
+            // get all players
+            // return to page
+        }
+
+        public void randomGameRequest() {
+            this.createGame("NL", "Lars");
+        }
+
+        public void createNewGame() {
+            this.createGame("NL", "Lars");
+        }
+
+        private boolean createGame(String letterset, String username2) {
+            Boolean gameCreated = false;
+
+            try {
+                Connection connection = Singleton.getInstance().getConnection();
+                Singleton.getInstance().getUser();
+
+            } catch (Exception e) {
+                throw new DbLoadException(e);
+            }
+
+            return gameCreated;
+        }
 }
