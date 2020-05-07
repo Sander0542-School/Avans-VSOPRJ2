774b70c1db83c39b57526cd7da6b589251b4c8a5
Lars Rijnen <Larsrijnen@gmail.com>
2020-05-07 14:27:26 +0200

    Getting maven error

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/NewController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/NewController.java
@@ -1,54 +1,59 @@
 package nl.avans.vsoprj2.wordcrex.controllers.game;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
 import nl.avans.vsoprj2.wordcrex.exceptions.DbLoadException;
 
+import javax.swing.*;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class NewController extends Controller {
-        public NewController() {
-//            Connection connection = Singleton.getInstance().getConnection();
-//
-//            try {
-//                PreparedStatement statement = connection.prepareStatement("SELECT * FROM `account` WHERE username != 'luc'");
-//                ResultSet resultSet = statement.executeQuery();
-//
-//                System.out.println(resultSet);
-//
-//                //Singleton.getInstance().getUser();
-//            } catch (SQLException e) {
-//                throw new DbLoadException(e);
-//            }
+    //todo 
+    //private list allnames
 
-            // get all players
-            // return to page
+    @FXML
+    private VBox suggestedAccounts;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        super.initialize(url, resourceBundle);
+
+        suggestedAccounts.managedProperty().bind(suggestedAccounts.visibleProperty());
+
+        loadAccounts("luc"); //TODO Add your username
+    }
+
+    private void loadAccounts(String Username) {
+        Connection connection = Singleton.getInstance().getConnection();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM `account` WHERE username != 'luc'");
+            ResultSet resultSet = statement.executeQuery();
+
+            System.out.println(resultSet);
+
+            //Singleton.getInstance().getUser();
+        } catch (SQLException e) {
+            throw new DbLoadException(e);
         }
+    }
 
+    public void randomGameRequest() {
+        this.createGame("NL", "Lars");
+    }
 
-        public void randomGameRequest() {
-            this.createGame("NL", "Lars");
-        }
+    public void createNewGame() {
+        this.createGame("NL", "Lars");
+    }
 
-        public void createNewGame() {
-            this.createGame("NL", "Lars");
-        }
-
-        private boolean createGame(String letterset, String username2) {
-            Boolean gameCreated = false;
-
-//            try {
-//                Connection connection = Singleton.getInstance().getConnection();
-//                Singleton.getInstance().getUser();
-//
-//            } catch (Exception e) {
-//                throw new DbLoadException(e);
-//            }
-
-            return gameCreated;
-        }
+    private void createGame(String letterset, String username2) {
+        Boolean gameCreated = false;
+    }
 }
