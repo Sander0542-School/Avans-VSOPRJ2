c4d0eb749a55ef0eb9b116e59315b8983ee93c0d
Lars Rijnen <Larsrijnen@gmail.com>
2020-05-07 14:27:13 +0200

    Found bug in routing

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/GamesController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/GamesController.java
@@ -46,25 +46,27 @@ public class GamesController extends Controller {
 
     public void newGamePage() {
         navigateTo("/views/game/new.fxml");
+    }
 
-//        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-//            alert.setTitle("Game request");
-//            alert.setHeaderText("Je kunt een nieuw spel starten met Sander0542");
-//
-//        ButtonType buttonTypeDecline = new ButtonType("Decline");
-//        ButtonType buttonTypeAccept = new ButtonType("Accept");
-//        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
-//
-//            alert.getButtonTypes().setAll(buttonTypeDecline, buttonTypeAccept, buttonTypeCancel);
-//
-//        Optional<ButtonType> result = alert.showAndWait();
-//            if (result.get() == buttonTypeDecline) {
-//            System.out.println("Decline game request");
-//        } else if (result.get() == buttonTypeAccept) {
-//            System.out.println("Accept game request");
-//        } else {
-//            // Cancel alert popup
-//        }
+    private void gameRequest() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Game request");
+        alert.setHeaderText("Je kunt een nieuw spel starten met Sander0542");
+
+        ButtonType buttonTypeDecline = new ButtonType("Decline");
+        ButtonType buttonTypeAccept = new ButtonType("Accept");
+        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(buttonTypeDecline, buttonTypeAccept, buttonTypeCancel);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeDecline) {
+            System.out.println("Decline game request");
+        } else if (result.get() == buttonTypeAccept) {
+            System.out.println("Accept game request");
+        } else {
+            // Cancel alert popup
+        }
     }
 
     private void loadGames(String username) {
