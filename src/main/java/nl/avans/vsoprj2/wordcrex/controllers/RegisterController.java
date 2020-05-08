5edb053b4e7e66947d544cf5be261e247348a23f
Sander Jochems <sanderjochems@hotmail.nl>
2020-05-08 11:29:55 +0200

    Implement Navigation on Register

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/RegisterController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/RegisterController.java
@@ -1,4 +1,10 @@
 package nl.avans.vsoprj2.wordcrex.controllers;
+public class RegisterController extends Controller {
+    public void handleBackButton() {
+        navigateTo("/views/index.fxml");
+    }
 
-public class RegisterController extends Controller  {
+    public void handleSignUpButton() {
+        navigateTo("/views/games.fxml");
+    }
 }
