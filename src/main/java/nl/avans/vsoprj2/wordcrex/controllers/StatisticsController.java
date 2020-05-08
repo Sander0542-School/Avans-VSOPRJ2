5096dc55764ec50b47b07d83b22b61375226caac
Sander Jochems <sanderjochems@hotmail.nl>
2020-05-08 12:04:00 +0200

    Implement Navigation on Statistics

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/StatisticsController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/StatisticsController.java
@@ -1,4 +1,15 @@
 package nl.avans.vsoprj2.wordcrex.controllers;
 
+import javafx.event.Event;
+import nl.avans.vsoprj2.wordcrex.controls.navigation.BottomBarItem;
+
 public class StatisticsController extends Controller  {
+
+    public void bottomBarNavigation(Event event) {
+        BottomBarItem bottomBarItem = (BottomBarItem) event.getSource();
+
+        if (bottomBarItem.getId().equals("games")) {
+            navigateTo("/views/games.fxml");
+        }
+    }
 }
