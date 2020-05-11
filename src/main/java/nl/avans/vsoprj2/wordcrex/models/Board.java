c12ae8e02fb79efe43fd1e3ebbb89abe682cc2df
Fnab <33625805+Fnab@users.noreply.github.com>
2020-05-11 22:39:06 +0200

    Revert "Revert "Added preset multipliers""
    This reverts commit 931389536499fb7b068592c7b9a6d7a3769a8eaa.

--- src/main/java/nl/avans/vsoprj2/wordcrex/models/Board.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/models/Board.java
@@ -1,5 +1,8 @@
 package nl.avans.vsoprj2.wordcrex.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 enum TileType {
     EMTPY,
     TWOLETTER,
@@ -12,14 +15,57 @@ enum TileType {
 
 public class Board {
     Tile[][] grid;
+    Map<String, TileType> predefinedTileTypes = new HashMap<String, TileType>();
 
     public Board() {
+        populatePredefinedTileTypes();
         grid = newBoard();
     }
 
-    private Tile[][] newBoard() {
-        return new Tile[14][14];
+    private void populatePredefinedTileTypes() {
+        String[] TWOLETTER = {"2,1", "6,1", "8,1", "12,1", "8,3", "6,6", "8,6", "2,7", "12,7", "6,8", "8,8", "8,11", "2,13", "6,13", "8,13", "12,13"};
+        String[] THREEWORD = {"4,0", "10,0", "0,2", "14,2", "3,4", "11,4", "3,10", "11,10", "0,12", "14,12", "4,14", "10,14"};
+        String[] FOURLETTER = {"7,0", "3,2", "11,2", "5,3", "9,3", "1,4", "13,4", "7,5", "7,9", "1,10", "13,10", "5,11", "9,11", "3,12", "11,12", "7,14"};
+        String[] FOURWORD = {"0,7", "14,7"};
+        String[] SIXLETTER = {"0,0", "14,0", "4,5", "10,5", "1,6", "13,6", "1,8", "13,8", "4,9", "10,9", "14,14", "0,14"};
+
+        for (String key : TWOLETTER) {
+            predefinedTileTypes.put(key, TileType.TWOLETTER);
+        }
+
+        for (String key : THREEWORD) {
+            predefinedTileTypes.put(key, TileType.THREEWORD);
+        }
+
+        for (String key : FOURLETTER) {
+            predefinedTileTypes.put(key, TileType.FOURLETTER);
+        }
+
+        for (String key : FOURWORD) {
+            predefinedTileTypes.put(key, TileType.FOURWORD);
+        }
+
+        for (String key : SIXLETTER) {
+            predefinedTileTypes.put(key, TileType.SIXLETTER);
+        }
     }
+
+    private Tile[][] newBoard() {
+        int gridSize = 14;
+        Tile[][] newGrid = new Tile[gridSize][gridSize];
+
+        for (int x = 0; x < gridSize; x++) {
+            for (int y = 0; y < gridSize; y++) {
+                newGrid[x][y] = new Tile(getTileTye(x, y));
+            }
+        }
+        return newGrid;
+    }
+
+    private TileType getTileTye(int x, int y) {
+        return predefinedTileTypes.containsKey(x + "," + y) ? predefinedTileTypes.get(x + "," + y) : TileType.EMTPY;
+    }
+
 
     public Character getValue(int x, int y) {
         Character returnValue;
@@ -31,7 +77,7 @@ public class Board {
         return grid;
     }
 
-    public void setTile(int x, int y, Character Value) {
+    public void setValue(int x, int y, Character Value) {
         grid[x][y].setValue(Value);
     }
 
