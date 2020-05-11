7cd9ce8efe3f854bbd29d836cb9501362daa9b10
Tommy Hosewol <t.hosewol@gmail.com>
2020-05-11 14:21:52 +0200

    Remove unused setters

--- src/main/java/nl/avans/vsoprj2/wordcrex/models/Statistic.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/models/Statistic.java
@@ -31,47 +31,23 @@ public class Statistic extends Model {
         return gamesWon;
     }
 
-    public void setGamesWon(String gamesWon) {
-        this.gamesWon = gamesWon;
-    }
-
     public String getGamesLost() {
         return gamesLost;
-    }
-
-    public void setGamesLost(String gamesLost) {
-        this.gamesLost = gamesLost;
     }
 
     public String getGamesTied() {
         return gamesTied;
     }
 
-    public void setGamesTied(String gamesTied) {
-        this.gamesTied = gamesTied;
-    }
-
     public String getGamesLeft() {
         return gamesLeft;
-    }
-
-    public void setGamesLeft(String gamesLeft) {
-        this.gamesLeft = gamesLeft;
     }
 
     public String getTopGameScore() {
         return topGameScore;
     }
 
-    public void setTopGameScore(String topGameScore) {
-        this.topGameScore = topGameScore;
-    }
-
     public String getTopWordScore() {
         return topWordScore;
-    }
-
-    public void setTopWordScore(String topWordScore) {
-        this.topWordScore = topWordScore;
     }
 }
