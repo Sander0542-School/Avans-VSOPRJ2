eedb97a3c10b05d1457be54c735064f63074cc26
Fnab <33625805+Fnab@users.noreply.github.com>
2020-05-11 21:20:20 +0200

    Getters and setters

--- src/main/java/nl/avans/vsoprj2/wordcrex/models/Board.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/models/Board.java
@@ -4,14 +4,46 @@ public class Board {
     Tile[][] grid;
 
     public Board() {
-        grid = new Tile[14][14];
+        grid = newBoard();
+    }
+
+    private Tile[][] newBoard(){
+        return new Tile[14][14];
+    }
+
+    public Character getValue(int x, int y){
+        Character returnValue;
+        returnValue = grid[x][y].getValue();
+        return returnValue;
+    }
+
+    public Tile[][] getGrid(){
+        return grid;
+    }
+
+    public void setTile(int x, int y, Character Value){
+        grid[x][y].setValue(Value);
     }
 
 }
 
 class Tile{
-    public int x;
-    public int y;
-    public Character value;
-    public int tileType;
+    private Character value;
+    private int tileType;
+
+    public Tile(int type){
+        tileType = type;
+    }
+
+    public Character getValue() {
+        return value;
+    }
+
+    public void setValue(Character value) {
+        this.value = value;
+    }
+
+    public int getTileType() {
+        return tileType;
+    }
 }
