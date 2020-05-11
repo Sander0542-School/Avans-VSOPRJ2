8da1dfd525dd2a86ba44217ef90d379f9b9fcfa0
Fnab <33625805+Fnab@users.noreply.github.com>
2020-05-11 21:09:08 +0200

    Added Tile Class and a Grid

--- src/main/java/nl/avans/vsoprj2/wordcrex/models/Board.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/models/Board.java
@@ -1,5 +1,17 @@
 package nl.avans.vsoprj2.wordcrex.models;
 
 public class Board {
+    Tile[][] grid;
 
+    public Board() {
+        grid = new Tile[14][14];
+    }
+
+}
+
+class Tile{
+    public int x;
+    public int y;
+    public Character value;
+    public int tileType;
 }
