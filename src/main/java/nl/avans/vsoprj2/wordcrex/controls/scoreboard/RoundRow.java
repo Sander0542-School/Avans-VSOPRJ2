8168a635d509b0a1dd811fcc814fc1620bd657f1
daanh432 <daan@daanhendriks.nl>
2020-05-14 16:16:21 +0200

    Font weight workaround

--- src/main/java/nl/avans/vsoprj2/wordcrex/controls/scoreboard/RoundRow.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controls/scoreboard/RoundRow.java
@@ -4,12 +4,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import nl.avans.vsoprj2.wordcrex.models.ScoreboardRound;
 
 import java.io.IOException;
 
 public class RoundRow extends VBox {
+    private ScoreboardRound scoreboardRound;
+
     @FXML
     private Text round;
     @FXML
@@ -42,6 +45,9 @@ public class RoundRow extends VBox {
 
         try {
             loader.load();
+
+            this.player1Name.managedProperty().bind(this.player1Name.visibleProperty());
+            this.player2Name.managedProperty().bind(this.player2Name.visibleProperty());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -49,29 +55,30 @@ public class RoundRow extends VBox {
 
     public RoundRow(ScoreboardRound scoreboardRound) {
         this();
-        this.render(scoreboardRound);
+        this.scoreboardRound = scoreboardRound;
+        this.render();
     }
 
-    public void render(ScoreboardRound scoreboardRound) {
-        int totalScorePlayerOne = scoreboardRound.getScorePlayerOne() + scoreboardRound.getBonusPlayerOne();
-        int totalScorePlayerTwo = scoreboardRound.getScorePlayerTwo() + scoreboardRound.getBonusPlayerTwo();
-        this.round.setText(String.valueOf(scoreboardRound.getTurnId()));
-        this.letterSet.setText(scoreboardRound.getHandContent());
-        this.player1Name.setText(scoreboardRound.getUsernamePlayerOne());
-        this.player2Name.setText(scoreboardRound.getUsernamePlayerTwo());
+    public void render() {
+        int totalScorePlayerOne = this.scoreboardRound.getScorePlayerOne() + this.scoreboardRound.getBonusPlayerOne();
+        int totalScorePlayerTwo = this.scoreboardRound.getScorePlayerTwo() + this.scoreboardRound.getBonusPlayerTwo();
+        this.round.setText(String.valueOf(this.scoreboardRound.getTurnId()));
+        this.letterSet.setText(this.scoreboardRound.getHandContent());
+        this.player1Name.setText(this.scoreboardRound.getUsernamePlayerOne());
+        this.player2Name.setText(this.scoreboardRound.getUsernamePlayerTwo());
         this.player1Score.setText(String.valueOf(totalScorePlayerOne));
         this.player2Score.setText(String.valueOf(totalScorePlayerTwo));
-        this.player1Word.setText(scoreboardRound.getWordPlayerOne());
-        this.player2Word.setText(scoreboardRound.getWordPlayerTwo());
+        this.player1Word.setText(this.scoreboardRound.getWordPlayerOne());
+        this.player2Word.setText(this.scoreboardRound.getWordPlayerTwo());
         this.player1Time.setText("--:--"); // Time isn't present in the database?
         this.player2Time.setText("--:--"); // Time isn't present in the database?
 
         if (totalScorePlayerOne > totalScorePlayerTwo) {
-            this.player1Name.setStyle("-fx-font-weight: bold");
-            this.player2Name.setStyle("-fx-font-weight: normal");
+            this.player1Name.setFont(new Font(16.0));
+            this.player2Name.setFont(new Font(14.0));
         } else {
-            this.player1Name.setStyle("-fx-font-weight: normal");
-            this.player2Name.setStyle("-fx-font-weight: bold");
+            this.player1Name.setFont(new Font(14.0));
+            this.player2Name.setFont(new Font(16.0));
         }
     }
 }
