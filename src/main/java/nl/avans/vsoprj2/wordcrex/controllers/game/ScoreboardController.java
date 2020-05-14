d0891cefa23072e46fc9807a57b138ff884765fe
daanh432 <daan@daanhendriks.nl>
2020-05-14 09:53:36 +0200

    Test game instance fetching

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/ScoreboardController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/ScoreboardController.java
@@ -2,12 +2,17 @@ package nl.avans.vsoprj2.wordcrex.controllers.game;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
 import nl.avans.vsoprj2.wordcrex.controls.scoreboard.RoundRow;
 import nl.avans.vsoprj2.wordcrex.models.Game;
 import nl.avans.vsoprj2.wordcrex.models.ScoreboardRound;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -35,7 +40,16 @@ public class ScoreboardController extends Controller {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        Connection connection = Singleton.getInstance().getConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM game WHERE game_id = 502");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                this.setGame(new Game(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
