cbc25844a9b7798099174c52002241be5fa7be41
MaxvdVossenberg <maxvossenberg@gmail.com>
2020-05-15 15:35:09 +0200

    Implement basic user input

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
@@ -1,7 +1,7 @@
 package nl.avans.vsoprj2.wordcrex.controllers.game;
 
-import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
 import nl.avans.vsoprj2.wordcrex.exceptions.DbLoadException;
@@ -11,24 +11,50 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class BoardController extends Controller {
-    @FXML
-    private void confirmLettersButtonClicked() {
-        Alert testAlert = new Alert(Alert.AlertType.INFORMATION, "confirmLetterButtonClicked()");
-        testAlert.setTitle("TEST");
-        testAlert.show();
+    private Game game;
+    public void confirmLettersButtonClicked() {
+        Alert confirmationDialog = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmationDialog.setTitle("Bevestig Woord");
+        confirmationDialog.setHeaderText("Weet je zeker dat je dit woord wil spelen?");
+        Optional<ButtonType> dialogResult = confirmationDialog.showAndWait();
+
+        if(dialogResult.isPresent())
+        if (dialogResult.get() == ButtonType.OK) {
+            String word = "goed"; //TODO: Create getWord() Method
+            if(this.checkWord(word)) {
+                //TODO: inplement score calculations
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Goed woord.");
+                alert.setTitle("Correcte dingen jongeman!");
+                alert.showAndWait();
+            } else {
+                //Throws alert if word is not correct
+                Alert invalidWordDialog = new Alert(Alert.AlertType.ERROR, "Dit is geen geldig woord.");
+                invalidWordDialog.setTitle("Fout Woord");
+                invalidWordDialog.showAndWait();
+            }
+        }
     }
 
+    /**
+     * This method needs to be called in the BeforeNavigation.
+     * See following link : https://github.com/daanh432/Avans-VSOPRJ2/pull/35#discussion_r420678493
+     *
+     * @param game - The game id from the database to display.
+     */
     public void setGame(Game game) {
+        if (game == null) throw new IllegalArgumentException("Game can not be null");
+        this.game = game;
     }
 
-    public boolean checkWord(Game game, String word) {
+    public boolean checkWord(String word) {
         Connection connection = Singleton.getInstance().getConnection();
         try {
             PreparedStatement statement = connection.prepareStatement("SELECT EXISTS(SELECT * FROM dictionary WHERE word = ? AND letterset_code = ? AND state = 'accepted');");
             statement.setString(1, word);
-            statement.setString(2, game.getLettersetCode());
+            statement.setString(2, this.game.getLettersetCode());
             ResultSet result = statement.executeQuery();
             result.next();
             return result.getBoolean(1);
