abbed0eb0436dc0a46d7e63f2a27967cb1d50557
Fnab <33625805+Fnab@users.noreply.github.com>
2020-05-19 11:13:36 +0200

    Update Board.java
    Read boardPlayers tabel from Database
    and Fill boardModel with resultset

--- src/main/java/nl/avans/vsoprj2/wordcrex/models/Board.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/models/Board.java
@@ -21,15 +21,15 @@ public class Board {
         SIXLETTER
     }
 
-    private Tile[][] grid;
-    private Map<String, TileType> predefinedTileTypes = new HashMap<String, TileType>();
+    private final Tile[][] grid;
+    private final Map<String, TileType> predefinedTileTypes = new HashMap<String, TileType>();
 
     public Board() {
         this.grid = this.newBoard();
     }
 
     private Tile[][] newBoard() {
-        int gridSize = 14;
+        int gridSize = 15;
         Tile[][] newGrid = new Tile[gridSize][gridSize];
 
         Connection connection = Singleton.getInstance().getConnection();
@@ -41,9 +41,8 @@ public class Board {
             while (result.next()) {
                 int xCord = result.getInt(1);
                 int YCord = result.getInt(2);
-                String type =  result.getString(3);
+                String type = result.getString(3);
                 newGrid[xCord][YCord] = new Tile(this.getTileType(type));
-
             }
             return newGrid;
         } catch (SQLException e) {
@@ -51,8 +50,8 @@ public class Board {
         }
     }
 
-    private TileType getTileType(String type){
-        switch (type){
+    private TileType getTileType(String type) {
+        switch (type) {
             case "*":
                 return TileType.START;
             case "2L":
@@ -67,6 +66,42 @@ public class Board {
                 return TileType.SIXLETTER;
             default:
                 return TileType.NORMAL;
+        }
+    }
+
+    public void updateBoard(int GameId) {
+        Connection connection = Singleton.getInstance().getConnection();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM `gelegd` WHERE game_id = ?");
+            statement.setInt(1, GameId);
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                String word = result.getString(1);
+                String xValuesString = result.getString(2);
+                String yValuesString = result.getString(3);
+
+                String[] characters = word.split(",");
+                int[] xValues = new int[characters.length];
+                int[] yValues = new int[characters.length];
+
+                String[] xValue = xValuesString.split(",");
+                for (int i = 0; i < xValue.length; i++) {
+                    xValues[i] = Integer.parseInt(xValue[i]);
+                }
+
+                String[] yValue = yValuesString.split(",");
+                for (int i = 0; i < yValue.length; i++) {
+                    yValues[i] = Integer.parseInt(yValue[i]);
+                }
+
+                for (int i = 0; i < characters.length; i++) {
+                    this.grid[xValues[i]][yValues[i]].setValue(characters[i].charAt(0));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DbLoadException(e);
         }
     }
 
