63d1a372b7a3321a6d38355679cfec3791bc4fc3
Tommy Hosewol <t.hosewol@gmail.com>
2020-05-19 16:55:43 +0200

    Add username

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/SettingsController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/SettingsController.java
@@ -1,8 +1,20 @@
 package nl.avans.vsoprj2.wordcrex.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import nl.avans.vsoprj2.wordcrex.Singleton;
 
 public class SettingsController extends Controller {
+
+    @FXML
+    public Label username;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        super.initialize(url, resourceBundle);
+
+        this.username.setText(Singleton.getInstance().getUser().getUsername());
+    }
 
     public void handleLogoutAction() {
         Singleton.getInstance().setUser(null);
