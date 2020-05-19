a689c0fe7a30d521cd72ffd664c8dd5ae5676ead
Lars Rijnen <Larsrijnen@gmail.com>
2020-05-19 16:17:06 +0200

    Working pass only waiting on wordsInHand

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/BoardController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
+import nl.avans.vsoprj2.wordcrex.controls.games.SuggestedAccount;
 import nl.avans.vsoprj2.wordcrex.exceptions.DbLoadException;
 import nl.avans.vsoprj2.wordcrex.models.Account;
 import nl.avans.vsoprj2.wordcrex.models.Game;
@@ -51,38 +52,74 @@ public class BoardController extends Controller {
         Optional<ButtonType> result = alert.showAndWait();
 
         if (result.get() == buttonTypeOk) {
-//            this.passGame();
+            this.passGame();
         }
     }
 
     private void passGame() {
         Connection connection = Singleton.getInstance().getConnection();
         String currentUsername = Singleton.getInstance().getUser().getUsername();
-        boolean isPlayer1 = this.game.getUsernamePlayer1().equals(Singleton.getInstance().getUser().getUsername());
+        boolean isPlayer1 = this.game.getUsernamePlayer1().equals(currentUsername);
+        String typePlayer1 = "notSet";
+        String typePlayer2 = "notSet";
 
-        //TODO Check Turn status
-        //SELECT (SELECT turnaction_type FROM turnplayer1 WHERE game_id = 546 ORDER BY turn_id DESC) AS type_player1, (SELECT turnaction_type FROM turnplayer2 WHERE game_id = 546 ORDER BY turn_id DESC) AS type_player2
-
+        //Getting turn information of players
         try {
-            if (isPlayer1) {
-                PreparedStatement statement = connection.prepareStatement("INSERT INTO turnplayer1(game_id, turn_id, username_player1, bonus, score, turnaction_type) VALUES (?, (SELECT (IFnull(MAX(turn_id), 0) + 1) AS next_turn FROM `turnplayer1` WHERE game_id = ?), ?, 0, 0, 'pass')");
+            PreparedStatement statement = connection.prepareStatement("SELECT (SELECT turnaction_type FROM turnplayer1 WHERE game_id = ? AND turn_id = (SELECT MAX(turn_id) AS turn FROM `turn` WHERE game_id = ?) ORDER BY turn_id DESC) AS type_player1, (SELECT turnaction_type FROM turnplayer2 WHERE game_id = ? AND turn_id = (SELECT MAX(turn_id) AS turn FROM `turn` WHERE game_id = ?) ORDER BY turn_id DESC) AS type_player2");
+            statement.setInt(1, this.game.getGameId());
+            statement.setInt(2, this.game.getGameId());
+            statement.setInt(3, this.game.getGameId());
+            statement.setInt(4, this.game.getGameId());
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                typePlayer1 = resultSet.getString("type_player1");
+                typePlayer2 = resultSet.getString("type_player2");
+            }
+        } catch (SQLException e) {
+            throw new DbLoadException(e);
+        }
+
+        if (isPlayer1 && typePlayer1 == null) {
+            try {
+                PreparedStatement statement = connection.prepareStatement("INSERT INTO turnplayer1(game_id, turn_id, username_player1, bonus, score, turnaction_type) VALUES (?, (SELECT (IFnull(MAX(turn_id), 0) + 1) AS next_turn FROM turnplayer1 t2 WHERE game_id = ?), ?, 0, 0, 'pass')");
                 statement.setInt(1, this.game.getGameId());
                 statement.setInt(2, this.game.getGameId());
                 statement.setString(3, currentUsername);
 
-                statement.execute();
-            } else {
+                statement.executeUpdate();
+            } catch (SQLException e) {
+                throw new DbLoadException(e);
+            } finally {
+                if (typePlayer2 != null && typePlayer2.equals("pass")) {
+                    this.giveNewLetterInHand();
+                }
             }
+        } else if (!isPlayer1 && typePlayer2 == null) {
+            try {
+                PreparedStatement statement = connection.prepareStatement("INSERT INTO turnplayer2(game_id, turn_id, username_player1, bonus, score, turnaction_type) VALUES (?, (SELECT (IFnull(MAX(turn_id), 0) + 1) AS next_turn FROM turnplayer2 t2 WHERE game_id = ?), ?, 0, 0, 'pass')");
+                statement.setInt(1, this.game.getGameId());
+                statement.setInt(2, this.game.getGameId());
+                statement.setString(3, currentUsername);
 
-
-        } catch (SQLException e) {
-            throw new DbLoadException(e);
+                statement.executeUpdate();
+            } catch (SQLException e) {
+                throw new DbLoadException(e);
+            } finally {
+                if (typePlayer1 != null && typePlayer1.equals("pass")) {
+                    this.giveNewLetterInHand();
+                }
+            }
+        } else {
+            System.out.println("Je hebt deze beurt al iets gedaan");
         }
     }
 
     private void giveNewLetterInHand() {
         //TODO Give new letters
         //TODO Start new turn
+        //TODO Check if game end
     }
 
     public boolean isExistingWord(String word) {
