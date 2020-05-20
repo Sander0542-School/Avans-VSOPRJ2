4495c7ed16482a4f09f2f1606e602b700462958b
daanh432 <daan@daanhendriks.nl>
2020-05-20 15:48:27 +0200

    Removed extra whitelines

--- src/main/java/nl/avans/vsoprj2/wordcrex/controls/gameboard/LetterTile.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controls/gameboard/LetterTile.java
@@ -5,26 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import nl.avans.vsoprj2.wordcrex.models.Board;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LetterTile extends AnchorPane implements Initializable {
-
     @FXML
     private Label worth;
     @FXML
     private Label letter;
-
 
     public LetterTile(Character character, int worth) {
         this();
 
         this.letter.setText(character.toString());
         this.worth.setText(String.valueOf(worth));
-
     }
 
     public LetterTile() {
@@ -40,7 +36,6 @@ public class LetterTile extends AnchorPane implements Initializable {
             throw new RuntimeException(e);
         }
     }
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
