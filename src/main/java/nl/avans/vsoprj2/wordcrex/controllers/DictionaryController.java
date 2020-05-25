3f4f4f077df425a9eadf4f342f342c60f4c14cec
Fnab <33625805+Fnab@users.noreply.github.com>
2020-05-25 17:01:30 +0200

    Outline

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/DictionaryController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/DictionaryController.java
@@ -3,6 +3,21 @@ package nl.avans.vsoprj2.wordcrex.controllers;
 public class DictionaryController extends Controller{
 
     public DictionaryController(){
-        System.out.println("Controller");
+        this.fillLanguageDropdown();
+    }
+
+    private void fillLanguageDropdown(){
+        //SELECT * FROM `letterset`
+    }
+
+    public void getPending(){
+        //SELECT * FROM `dictionary` WHERE `letterset_code` = ? AND state = 'pending'
+    }
+
+    public void submit(){
+        //validation
+
+        //Insert
+        //INSERT INTO `dictionary`(`word`, `letterset_code`, `state`, `username`) VALUES ([value-1],[value-2],[value-3],[value-4])
     }
 }
