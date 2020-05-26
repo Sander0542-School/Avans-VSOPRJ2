1e3dec3ad477099b1abe277e721c3f44a33325a8
Fnab <33625805+Fnab@users.noreply.github.com>
2020-05-26 11:22:24 +0200

    First 3 fields of form

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/information/DictionaryController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/information/DictionaryController.java
@@ -1,7 +1,5 @@
 package nl.avans.vsoprj2.wordcrex.controllers.information;
 
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -10,14 +8,14 @@ import javafx.scene.control.TextField;
 import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
 import nl.avans.vsoprj2.wordcrex.exceptions.DbLoadException;
-import nl.avans.vsoprj2.wordcrex.models.Tile;
 
-import javax.swing.plaf.synth.SynthLookAndFeel;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.ResourceBundle;
 
 public class DictionaryController extends Controller {
@@ -25,7 +23,6 @@ public class DictionaryController extends Controller {
     private void handleBackButton() {
         this.navigateTo("/views/information.fxml");
     }
-
     @FXML
     private TextField username;
     @FXML
@@ -41,6 +38,8 @@ public class DictionaryController extends Controller {
     @FXML
     private ComboBox<String> language;
 
+    private Dictionary<String, String> languages = new Hashtable<String, String>();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.language.getItems().removeAll(this.language.getItems());
@@ -48,10 +47,10 @@ public class DictionaryController extends Controller {
     }
 
 
-    public DictionaryController(){
+    public DictionaryController() {
     }
 
-    private String[] Languages(){
+    private String[] Languages() {
         Connection connection = Singleton.getInstance().getConnection();
         try {
             PreparedStatement statement = connection.prepareStatement("SELECT * FROM letterset"); //returns 4 results instead of expected 2
@@ -63,7 +62,8 @@ public class DictionaryController extends Controller {
             result.beforeFirst();
 
             while (result.next()) {
-               returnValue[result.getRow() -1] = result.getString(2);
+                returnValue[result.getRow() - 1] = result.getString(2);
+                this.languages.put(result.getString(2),result.getString(1));
             }
             return returnValue;
         } catch (SQLException e) {
@@ -71,7 +71,7 @@ public class DictionaryController extends Controller {
         }
     }
 
-    public void getPending(String word, String code){
+    public void getPending(String word, String code) {
         Connection connection = Singleton.getInstance().getConnection();
         try {
             PreparedStatement statement = connection.prepareStatement("SELECT * FROM dictionary WHERE word = ? AND letterset_code = ? AND state = 'pending'");
@@ -86,8 +86,33 @@ public class DictionaryController extends Controller {
         }
     }
 
-    public void submit(){
+    public void submit() {
         //validation
+        if (this.username.getText().trim().isEmpty()){
+            this.error.setVisible(true);
+            this.error.setText("username");
+            return;
+        };
+
+        if (this.word.getText().trim().isEmpty()){
+            this.error.setVisible(true);
+            this.error.setText("word");
+            return;
+        };
+
+        if (this.comment.getText().trim().isEmpty()){
+            this.error.setVisible(true);
+            this.error.setText("comment");
+            return;
+        };
+
+
+
+
+       // this.add;
+        //this.delete;
+     //   this.language;
+
 
         //Insert
         //INSERT INTO `dictionary`(`word`, `letterset_code`, `state`, `username`) VALUES ([value-1],[value-2],[value-3],[value-4])
