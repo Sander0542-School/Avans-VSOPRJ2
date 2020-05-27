e8b78cb1904b3721bb2aec862b57d999df8d6bcf
Fnab <33625805+Fnab@users.noreply.github.com>
2020-05-27 06:23:41 +0200

    Admin Dictionary added to navigation

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/InformationController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/InformationController.java
@@ -1,6 +1,7 @@
 package nl.avans.vsoprj2.wordcrex.controllers;
 
 import javafx.fxml.FXML;
+import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.information.DictionaryController;
 
 public class InformationController extends Controller {
@@ -9,15 +10,32 @@ public class InformationController extends Controller {
         this.navigateTo("/views/games.fxml");
     }
     @FXML
-    private void handleDictionary() { this.navigateTo("/views/information/dictionary.fxml", new NavigationListener() {
-            @Override
-            public void beforeNavigate(Controller controller) {
-                DictionaryController dictionaryController = new DictionaryController();
-            }
-            @Override
-            public void afterNavigate(Controller controller) {
+    private void handleDictionary() {
 
-            }
-        });
+        String userRole = Singleton.getInstance().getUser().getRole(); // returns null
+
+        if( userRole == "administrator"){
+            this.navigateTo("/views/information/adminDictionaryPage.fxml", new NavigationListener() {
+                @Override
+                public void beforeNavigate(Controller controller) {
+                    DictionaryController dictionaryController = new DictionaryController();
+                }
+                @Override
+                public void afterNavigate(Controller controller) {
+
+                }
+            });
+        }else {
+            this.navigateTo("/views/information/dictionary.fxml", new NavigationListener() {
+                @Override
+                public void beforeNavigate(Controller controller) {
+                    DictionaryController dictionaryController = new DictionaryController();
+                }
+                @Override
+                public void afterNavigate(Controller controller) {
+
+                }
+            });
+        }
     }
 }
