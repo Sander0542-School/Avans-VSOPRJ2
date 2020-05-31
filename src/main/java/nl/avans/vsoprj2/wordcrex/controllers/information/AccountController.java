bbf91766c070cd9782ed562e69fd040f622ea51c
MaxvdVossenberg <maxvossenberg@gmail.com>
2020-05-31 17:33:23 +0200

    Added name for clarity

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/information/AccountController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/information/AccountController.java
@@ -3,7 +3,11 @@ package nl.avans.vsoprj2.wordcrex.controllers.information;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import nl.avans.vsoprj2.wordcrex.Singleton;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class AccountController extends Controller {
     @FXML
@@ -20,6 +24,13 @@ public class AccountController extends Controller {
 
     @FXML
     private Label username;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        super.initialize(url, resourceBundle);
+
+        this.username.setText(Singleton.getInstance().getUser().getUsername());
+    }
 
     @FXML
     private void handleBackButton() {
