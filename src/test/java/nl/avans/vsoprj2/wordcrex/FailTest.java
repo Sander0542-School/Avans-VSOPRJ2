376840230c68637511024ff22eedfb437890b2dd
Sander Jochems <sanderjochems@hotmail.nl>
2020-06-01 16:54:15 +0200

    Create FailTest.java

---
+++ src/test/java/nl/avans/vsoprj2/wordcrex/FailTest.java
@@ -0,0 +1,9 @@
+package nl.avans.vsoprj2.wordcrex;
+
+import junit.framework.TestCase;
+
+public class FailTest extends TestCase {
+    public void wrongTest() {
+        assertEquals(1,2);
+    }
+}
