c7b8e5192cc3a13543e3c9efe37643891a68c1ad
Sander Jochems <sanderjochems@hotmail.nl>
2020-06-01 16:58:53 +0200

    Delete FailTest.java

--- src/test/java/nl/avans/vsoprj2/wordcrex/FailTest.java
+++
@@ -1,9 +0,0 @@
-package nl.avans.vsoprj2.wordcrex;
-
-import junit.framework.TestCase;
-
-public class FailTest extends TestCase {
-    public void testWrong() {
-        assertEquals(1,2);
-    }
-}
