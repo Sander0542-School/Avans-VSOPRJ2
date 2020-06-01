e0fc064d0ab4aa2621f09e16f87b1bdeeb259a46
Sander Jochems <sanderjochems@hotmail.nl>
2020-06-01 16:53:14 +0200

    Create AccountModelTest

---
+++ src/test/java/nl/avans/vsoprj2/wordcrex/AccountModelTest.java
@@ -0,0 +1,25 @@
+package nl.avans.vsoprj2.wordcrex;
+
+import junit.framework.TestCase;
+import nl.avans.vsoprj2.wordcrex.models.Account;
+
+public class AccountModelTest extends TestCase {
+    public void testFromUsername() {
+        String username = "jagermeester";
+
+        Account account = Account.fromUsername(username);
+
+        assertNotNull(account);
+        assertEquals(username, account.getUsername());
+    }
+
+    public void testFromUsernamePassword() {
+        String username = "jagermeester";
+        String password = "rrr";
+
+        Account account = Account.fromUsernamePassword(username, password);
+
+        assertNotNull(account);
+        assertEquals(username, account.getUsername());
+    }
+}
