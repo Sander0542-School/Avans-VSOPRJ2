b7360ef623288374f7f1aae80748d7a2ed2f232c
Kasteel <33625805+Fnab@users.noreply.github.com>
2020-06-01 11:29:43 +0200

    Update src/main/java/nl/avans/vsoprj2/wordcrex/controllers/information/DictionaryController.java
    Co-authored-by: Tommy Hosewol <t.hosewol@gmail.com>

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/information/DictionaryController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/information/DictionaryController.java
@@ -130,7 +130,7 @@ public class DictionaryController extends Controller {
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 this.error.setVisible(true);
-                this.error.setText("Woord is eerder toegevoegd");
+                this.error.setText("Dit woord zit al in ons woordenboek!");
                 return;
             }
 
