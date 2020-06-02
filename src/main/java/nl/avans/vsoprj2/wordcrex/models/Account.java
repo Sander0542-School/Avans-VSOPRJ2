fbc9903a282764fac3eea7df793d8f5c35825b82
daanh432 <daan@daanhendriks.nl>
2020-06-02 19:48:06 +0200

    Added global debug exception handler and alerts

--- src/main/java/nl/avans/vsoprj2/wordcrex/models/Account.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/models/Account.java
@@ -21,6 +21,37 @@ public class Account extends Model {
         super(resultSet);
     }
 
+    public static Account fromUsernamePassword(String username, String password) {
+        Connection connection = Singleton.getInstance().getConnection();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT username FROM account WHERE username = ? AND password = ?");
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet result = statement.executeQuery();
+
+            if (result.next()) {
+                return new Account(result);
+            }
+
+        } catch (SQLException e) {
+            WordCrex.handleException(e);
+
+            return null;
+        }
+
+        return null;
+    }
+
+    /**
+     * Fetches all the playing games you have access to
+     *
+     * @return list of the games that should be rendered in the playing games container
+     */
+    public List<Game> getPlayingGames() {
+        return this.getPlayingGames(false);
+    }
+
     /**
      * Fetches all the game requests you have access to
      *
@@ -44,17 +75,23 @@ public class Account extends Model {
             }
             return result;
         } catch (SQLException e) {
+            WordCrex.handleException(e);
+
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR, "Iets is er fout gegaan bij het ophalen van de aangevraagde spellen.");
+            errorAlert.setHeaderText(null);
+            errorAlert.showAndWait();
+
             return new ArrayList<>();
         }
     }
 
     /**
-     * Fetches all the playing games you have access to
+     * Fetches all the finished games you have access to
      *
-     * @return list of the games that should be rendered in the playing games container
+     * @return list of the games that should be rendered in the finished games container
      */
-    public List<Game> getPlayingGames() {
-        return this.getPlayingGames(false);
+    public List<Game> getFinishedGames() {
+        return this.getFinishedGames(false);
     }
 
     /**
@@ -89,17 +126,25 @@ public class Account extends Model {
             }
             return result;
         } catch (SQLException e) {
+            WordCrex.handleException(e);
+
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR, "Iets is er fout gegaan bij het ophalen van de lopende spellen.");
+            errorAlert.setHeaderText(null);
+            errorAlert.showAndWait();
+
             return new ArrayList<>();
         }
     }
 
-    /**
-     * Fetches all the finished games you have access to
-     *
-     * @return list of the games that should be rendered in the finished games container
-     */
-    public List<Game> getFinishedGames() {
-        return this.getFinishedGames(false);
+    public String getUsername() {
+        return this.username;
+    }
+
+    public enum Role {
+        PLAYER,
+        OBSERVER,
+        MODERATOR,
+        ADMINISTRATOR
     }
 
     /**
@@ -132,19 +177,23 @@ public class Account extends Model {
             }
             return result;
         } catch (SQLException e) {
+            WordCrex.handleException(e);
+
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR, "Iets is er fout gegaan bij het ophalen van de afgelopen spellen.");
+            errorAlert.setHeaderText(null);
+            errorAlert.showAndWait();
+
             return new ArrayList<>();
         }
     }
 
-    public String getUsername() {
-        return this.username;
-    }
-
-    public enum Role {
-        PLAYER,
-        OBSERVER,
-        MODERATOR,
-        ADMINISTRATOR
+    /**
+     * Override for combobox
+     *
+     * @return the username in string format
+     */
+    public String toString() {
+        return this.getUsername();
     }
 
     public Statistic getStatistic() {
@@ -168,47 +217,15 @@ public class Account extends Model {
 
             if (resultSet.next()) return new Statistic(resultSet);
 
-        } catch (SQLException ex) {
-            WordCrex.handleException(ex);
+        } catch (SQLException e) {
+            WordCrex.handleException(e);
 
-            throw new DbLoadException(ex);
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR, "Iets is er fout gegaan bij het ophalen van je statistieken.");
+            errorAlert.setHeaderText(null);
+            errorAlert.showAndWait();
         }
 
         return null;
-    }
-
-    /**
-     * Override for combobox
-     *
-     * @return the username in string format
-     */
-    public String toString() {
-        return this.getUsername();
-    }
-
-    public boolean hasRole(Role role) {
-        Connection connection = Singleton.getInstance().getConnection();
-        try {
-            PreparedStatement userRolesStatement = connection.prepareStatement("SELECT `role` FROM `accountrole` WHERE `username` = ?");
-            userRolesStatement.setString(1, Singleton.getInstance().getUser().getUsername());
-            ResultSet roles = userRolesStatement.executeQuery();
-
-            while (roles.next()) {
-                if (Account.Role.valueOf(roles.getString("role").toUpperCase()).equals(role)) {
-                    return true;
-                }
-            }
-        } catch (SQLException ex) {
-            if (WordCrex.DEBUG_MODE) {
-                System.err.println(ex.getErrorCode());
-            } else {
-                Alert invalidWordDialog = new Alert(Alert.AlertType.ERROR, "Er is iets fout gegaan bij het ophalen van de rollen.");
-                invalidWordDialog.setTitle("Error");
-                invalidWordDialog.showAndWait();
-            }
-        }
-
-        return false;
     }
 
     public static Account fromUsername(String username) {
@@ -230,23 +247,26 @@ public class Account extends Model {
         return null;
     }
 
-    public static Account fromUsernamePassword(String username, String password) {
+    public boolean hasRole(Role role) {
         Connection connection = Singleton.getInstance().getConnection();
-
         try {
-            PreparedStatement statement = connection.prepareStatement("SELECT username FROM account WHERE username = ? AND password = ?");
-            statement.setString(1, username);
-            statement.setString(2, password);
-            ResultSet result = statement.executeQuery();
+            PreparedStatement userRolesStatement = connection.prepareStatement("SELECT `role` FROM `accountrole` WHERE `username` = ?");
+            userRolesStatement.setString(1, Singleton.getInstance().getUser().getUsername());
+            ResultSet roles = userRolesStatement.executeQuery();
 
-            if (result.next()) {
-                return new Account(result);
+            while (roles.next()) {
+                if (Account.Role.valueOf(roles.getString("role").toUpperCase()).equals(role)) {
+                    return true;
+                }
             }
-
         } catch (SQLException e) {
-            throw new DbLoadException(e);
+            WordCrex.handleException(e);
+
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR, "Iets is er fout gegaan bij het ophalen van je rollen.");
+            errorAlert.setHeaderText(null);
+            errorAlert.showAndWait();
         }
 
-        return null;
+        return false;
     }
 }
