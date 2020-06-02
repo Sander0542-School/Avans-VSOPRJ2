2efe77ccaa8cca08e8e87bb30d8ddbc83add8d4a
daanh432 <daan@daanhendriks.nl>
2020-06-02 19:32:34 +0200

    Added global debug exception handler and alerts

--- src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/ScoreboardController.java
+++ src/main/java/nl/avans/vsoprj2/wordcrex/controllers/game/ScoreboardController.java
@@ -1,10 +1,12 @@
 package nl.avans.vsoprj2.wordcrex.controllers.game;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import nl.avans.vsoprj2.wordcrex.Singleton;
+import nl.avans.vsoprj2.wordcrex.WordCrex;
 import nl.avans.vsoprj2.wordcrex.controllers.Controller;
 import nl.avans.vsoprj2.wordcrex.controls.scoreboard.RoundRow;
 import nl.avans.vsoprj2.wordcrex.models.Game;
@@ -94,7 +96,11 @@ public class ScoreboardController extends Controller {
                 this.scoreboardRounds.add(new ScoreboardRound(resultSet));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            WordCrex.handleException(e);
+
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR, "Iets is er fout gegaan bij het laden van de punten.");
+            errorAlert.setHeaderText(null);
+            errorAlert.showAndWait();
         }
     }
 
